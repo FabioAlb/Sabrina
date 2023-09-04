653a3c465bb29a9bd1af22b0b4cec0a9273fa63d
Fábio Gabriel Albuquerque Sanches <fabio.alb.dev@gmail.com>
2023-09-04 16:19:38 -0300

    nova interface

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ window.addEventListener('scroll', function (event) {
         botao.classList.add('visible')
     }
 })
+function startnav() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    } else{
+        menuMobile.classList.add('open');
+    }
+}
